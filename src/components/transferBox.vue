45ccb4531994014855f33e653cc2a551d7bfbfdd
KinXpeng <kinxpeng@163.com>
2023-07-13 23:00:35 +0800

    feat: 新增穿梭框

--- src/components/transferBox.vue
+++ src/components/transferBox.vue
@@ -1,43 +1,59 @@
 <template>
   <div class="transfer-content">
     <!-- 左侧穿梭框 -->
-    <div class="transfer-box">
+    <div class="transfer-box" :style="{ 'height': height + 'px' }">
       <div class="box-title">
-        <el-checkbox v-model="list1Title">列表1</el-checkbox>
-        <span>0/7</span>
+        <el-input placeholder="请输入内容" v-model="searchKey" size="mini">
+          <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        </el-input>
+        <div class="title-select">
+          <el-checkbox :value="transferList.length > 0 && selectedLength(transferList) == transferList.length"
+            @change="handleLeftChange" :disabled="transferList.length == 0"
+            :indeterminate="selectedLength(transferList) > 0 && selectedLength(transferList) != transferList.length">列表1</el-checkbox>
+          <span>{{ selectedLength(transferList) + '/' + transferList.length }}</span>
+        </div>
       </div>
       <div class="box-info">
-        <div class="info-item" v-for="item in 12" :key="item">
-          <el-checkbox v-model="list1Title">列表1</el-checkbox>
+        <div class="info-item" v-show="!searchKey || item.name.includes(searchKey)" v-for="(item, index) in transferList"
+          :key="index">
+          <el-checkbox v-model="item.selected">{{ item.name }}</el-checkbox>
         </div>
       </div>
     </div>
 
     <!-- 右侧穿梭框及箭头 -->
     <div class="transfer-right">
-      <div class="right-content" v-for="item in groupLength" :key="item">
+      <div class="right-content" v-for="(item, index) in groupList" :key="index"
+        :style="{ 'height': groupHeight + 'px' }">
         <div class="transfer-arrow">
-          <el-button type="primary" icon="el-icon-arrow-left"></el-button>
-          <el-button type="primary" icon="el-icon-arrow-right"></el-button>
+          <el-button :disabled="selectedLength(item.list) == 0" type="primary" icon="el-icon-arrow-left"
+            @click="handleLeftTransfer(index)"></el-button>
+          <el-button :disabled="selectedLength(transferList) == 0" type="primary" icon="el-icon-arrow-right"
+            @click="handleRightTransfer(index)"></el-button>
         </div>
-        <div class="transfer-box">
+        <div class="transfer-box" :style="{ 'height': groupHeight + 'px' }">
           <div class="box-title">
-            <el-checkbox v-model="list1Title">列表1</el-checkbox>
-            <span>0/7</span>
+            <el-input placeholder="请输入内容" v-model="item.search" size="mini">
+              <i slot="prefix" class="el-input__icon el-icon-search"></i>
+            </el-input>
+            <div class="title-select">
+              <el-checkbox :value="item.list.length > 0 && selectedLength(item.list) == item.list.length"
+                @change="value => handleRightChange(value, index)" :disabled="item.list.length == 0"
+                :indeterminate="selectedLength(item.list) > 0 && selectedLength(item.list) != item.list.length">{{
+                  item.name }}</el-checkbox>
+              <span>{{ selectedLength(item.list) + '/' + item.list.length }}</span>
+            </div>
           </div>
           <div class="box-info">
-            <div class="info-item" v-for="item1 in 3" :key="item1">
-              <el-checkbox v-model="list1Title">列表1</el-checkbox>
+            <div class="info-item" v-show="!item.search || item1.name.includes(item.search)"
+              v-for="(item1, index1) in item.list" :key="index1">
+              <el-checkbox v-model="item1.selected">{{ item1.name }}</el-checkbox>
             </div>
           </div>
         </div>
       </div>
 
     </div>
-
-    <!-- <div>
-      <el-button @click="handleAddList">点击</el-button>
-    </div> -->
   </div>
 </template>
 
@@ -57,6 +73,18 @@ export default {
       type: Number,
       default: 1,
     },
+
+    // 穿梭框高度
+    height: {
+      type: Number,
+      default: 600
+    },
+
+    // 分组名称
+    groupNames: {
+      type: Array,
+      default: () => []
+    }
   },
   computed: {
     transferList: {
@@ -67,25 +95,72 @@ export default {
         this.$emit("input", v);
       },
     },
+    // 右侧高度
+    groupHeight() {
+      const singleHeight = (this.height - 30) / this.groupLength
+      return singleHeight
+    },
   },
   data() {
     return {
-      list1Title: false,
+      searchKey: '', // 左侧搜索
+      groupList: [], // 右侧分组
+      originTransferList: [], // 原有的穿梭数组
     }
   },
 
   methods: {
-    handleAddList() {
-      this.transferList.push({})
+    // 选中的数量
+    selectedLength(list) {
+      const newArr = list.filter(item => item.selected)
+      return newArr.length
+    },
+
+    // 左侧全选
+    handleLeftChange(value) {
+      this.transferList.forEach(item => {
+        item.selected = value
+      })
+    },
+
+    // 右侧全选
+    handleRightChange(value, index) {
+      this.groupList[index].list?.forEach(item => {
+        item.selected = value
+      })
+    },
+
+    // 左箭头
+    handleLeftTransfer(index) {
+      const filterList = this.groupList[index].list.filter(item => item.selected)
+      this.groupList[index].list = this.groupList[index].list.filter(item => !item.selected)
+      filterList.forEach(item => item.selected = false)
+      this.transferList.push(...filterList)
+    },
+
+    // 右箭头
+    handleRightTransfer(index) {
+      const filterList = this.transferList.filter(item => item.selected)
+      this.transferList = this.transferList.filter(item => !item.selected)
+      filterList.forEach(item => item.selected = false)
+      this.groupList[index].list.push(...filterList)
     },
   },
   created() {
-    console.log(this.value);
+    // console.log(this.transferList);
+    this.groupNames.forEach(item => {
+      this.groupList.push({
+        name: item,
+        search: '',
+        list: []
+      })
+    })
   },
 }
 </script>
 <style lang="scss" scoped>
 .transfer-content {
+  position: relative;
   display: flex;
   align-items: center;
   width: 100%;
@@ -93,19 +168,29 @@ export default {
   // border: 1px solid red;
 
   .transfer-box {
-    width: 160px;
+    overflow: auto;
+    width: 200px;
     // min-height: 160px;
     border: 1px solid #eee;
     border-radius: 6px;
 
     .box-title {
+      position: sticky;
+      top: 0;
       display: flex;
-      align-items: center;
+      flex-direction: column;
       justify-content: space-between;
-      height: 50px;
+      height: 60px;
       background-color: #f5f7fa;
       border-bottom: 1px solid #eee;
-      padding: 0 16px;
+      padding: 6px 16px;
+      z-index: 9;
+
+      .title-select {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
     }
 
     .box-info {
@@ -118,7 +203,7 @@ export default {
   }
 
   .transfer-right {
-    width: calc(100% - 160px);
+    width: calc(100% - 200px);
     height: 100%;
 
     .right-content {
@@ -131,7 +216,7 @@ export default {
       }
 
       .transfer-arrow {
-        width: 160px;
+        width: 200px;
       }
     }
   }
--- src/views/transfer/transfer.vue
+++ src/views/transfer/transfer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="box">
-    <TransferBox v-model="list" :groupLength="4" />
+    <TransferBox v-model="list" :groupLength="4" :groupNames="names" />
   </div>
 </template>
 
@@ -14,25 +14,67 @@ export default {
     return {
       inputValue: '',
       list: [
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-      ]
+        {
+          selected: false,
+          name: '备选项1'
+        },
+        {
+          selected: false,
+          name: '备选项2'
+        },
+        {
+          selected: false,
+          name: '备选项3'
+        },
+        {
+          selected: false,
+          name: '备选项4'
+        },
+        {
+          selected: false,
+          name: '备选项5'
+        },
+        {
+          selected: false,
+          name: '备选项6'
+        },
+        {
+          selected: false,
+          name: '备选项7'
+        },
+        {
+          selected: false,
+          name: '备选项8'
+        },
+        {
+          selected: false,
+          name: '备选项9'
+        },
+        {
+          selected: false,
+          name: '备选项10'
+        },
+        {
+          selected: false,
+          name: '备选项11'
+        },
+        {
+          selected: false,
+          name: '备选项12'
+        },
+        {
+          selected: false,
+          name: '备选项13'
+        },
+      ],
+      names: ['分组1', '分组2', '分组3', '分组4']
     }
   }
 }
 </script>
 <style lang="scss" scoped>
 .box {
-  width: 480px;
+  width: 580px;
   // height: 1000px;
   padding: 0 20px;
   margin: 0 auto;
